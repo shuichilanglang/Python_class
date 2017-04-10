0b1a047cc3d601b7bac839dc87218fdde3e6b094
shuichilanglang <850055863@qq.com>
2017-04-10 09:05:38 +0800

    this is the first test

---
+++ hellopython.py
@@ -0,0 +1,216 @@
+import random
+import  os
+import sys
+import math
+
+# print("hello World")
+#
+# name = "James"
+# print(name)
+#
+# print("5+2", 5+2)
+# print("5*2", 5*2)
+# print("5/2", 5/2)
+# print("5**2", 5**2)
+# print("5//2", 5//2)
+#
+# quote = "\"Always remember you are unique"
+# print("\n" * 2)
+# multi_line_quote = '''just
+# like everyone else'''
+# print("%s %s %s" % ('I like the quote',quote,multi_line_quote))
+
+# ls = [0.1, 0.2, 0.3, 0.4, 0.5]
+# vel_x = ls[random.randrange(0, 5)]
+# vel_y = ls[random.randrange(0, 5)]
+# print (ls[1])
+# print vel_y
+# print vel_x
+
+# grocery_list=['juice ','Tomatoes','Potatoes','Banana']
+# print(grocery_list[1:3])
+#
+# other_events = ['Wash Car','Pick up Kids','Cash Check']
+#
+# to_do_list=[other_events,grocery_list]
+# print(to_do_list)
+# print((to_do_list[1]))
+# print((to_do_list[1][2]))
+
+#两种集合的赋值方式
+# list1=[balabala]
+# list2=list1 List2和list1对应同一个地址空间
+# list2=klist2[:] 这样两个对应的区域不一样 纯复制
+
+
+# grocery_list.append('Onions')
+# print(to_do_list)
+#
+# grocery_list.insert(1,"Pickle")
+# print(to_do_list)
+#
+# grocery_list.remove("Pickle")
+#
+# grocery_list.sort()
+# # grocery_list.reverse()
+#
+# print(grocery_list)
+# print(to_do_list)
+
+#
+# pi_tuple = (3,1,3,1,5,9)
+# print(pi_tuple)
+#
+# new_tuple = list(pi_tuple)
+# new_list = tuple(new_tuple)
+#
+# print(new_tuple)
+# print(new_list)
+
+# super_villains = {'Fiddler':'Isaac Bowin',
+        #                   'Captain Cold':'Leonard Snart',
+        #                   'Weather Wizard':'Mark Mardon',
+        #                   'Mirror Master':'Sam Scudder',
+        #                   'Pied Piper':'Thomas Peterson'}\
+        #
+        # print(super_villains['Captain Cold'])
+        #
+        # del super_villains['Fiddler']
+        #
+# super_villains['Pied Piper'] = 'Hartley Rathaway'
+#
+# print(len(super_villains))
+#
+# print(super_villains.get("pied Piper"))
+#
+# print(super_villains.keys())
+#
+# print(super_villains.values())
+
+
+# age = 21
+# if age > 16 :
+#     print(" You are old enough to drive")
+# else:
+#     print(" You are not old enough to driver")
+
+# random_num = random.randrange(0,100)
+#
+# while(random_num != 19):
+#     print(random_num)
+#     random_num = random.randrange(0, 100)
+
+# def addNumber(fNum, lNum):
+#     return fNum + lNum
+#
+# print addNumber('I like ','That')
+
+# print("What is your name ?")
+#
+# name = sys.stdin.readline()
+#
+# print ('My name is',name)
+# print "My name is " ,name
+
+# long_string = "I'll catch you if you fall - The Floor"
+#
+# print(long_string[0:4])
+#
+# print(long_string[-5:])
+#
+# print(long_string[:-5])
+#
+# print(long_string[0:4])
+#
+
+# test_file = open("test.txt", "wb")
+#
+# print(test_file.mode)
+#
+# print(test_file.name)
+#
+# test_file.write(bytes("Write sth in the test.txt"), "UTF-8")
+#
+# test_file.close()
+
+# test_file = open("test.txt", "r++")
+#
+# text_in_file = test_file.read()
+# print(text_in_file)
+#
+# a = input("Lenth=")
+# b = input("Width=")
+# # h = input("Height")
+#
+# print("The area is %.2f" %(a*b/2))
+#
+# a = input("Lenth1=")
+# b = input("Lenth2=")
+# c = input("angel=")
+# print("The area is %.2f" %(a*b*math.sin(math.pi*c/180)/2))
+#
+# n=input("input:")
+# print(type(n))
+# if n == 0:
+#     print("not even or uneven number")
+# if n%2 == 0:
+#     print("even number")
+# else:
+#     print("uneven number") or odd
+
+
+# def func1(name,type):
+#     """display the msg of pet"""
+#     print(" I have a "+type+".")
+#     print(" My "+type+" name is "+name)
+#
+# func1('jack','cat')
+# func1(type='dog',name='jones')
+
+
+# def func1(name):
+#     return name
+#
+# print func1('sb')
+
+# def getMi(n1,n2,n3):
+#     if n1>n2 :
+#         temp=n2
+#         if temp>n3 :
+#             temp=n3
+#     elif n1<=n2:
+#         temp=n1
+#         if temp>n3:
+#             temp=n3
+
+
+# def func1(fruit[:],type)
+
+# def func2(*fruit):
+#     print (fruit)
+#
+#
+# fruit = ['banana','Aplle','Waterm']
+# func2(fruit)
+
+# test=range(1,11)
+#
+# print test[1:3]
+# print test[2:4]
+# print test[7:9]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
